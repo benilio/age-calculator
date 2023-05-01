53d36e4e80e9fe2a620d0c2f207f9d0af2c75f23
Benício Oliveira <beafoliveira@gmail.com>
2023-05-01 19:10:00 -0300

    split parts into components

---
+++ src/components/Attribution.tsx
@@ -0,0 +1,23 @@
+import styles from '@/styles/Home.module.css'
+
+export const Attribution = () => {
+	return (
+		<div className={styles.attribution}>
+			Challenge by{' '}
+			<a
+				href='https://www.frontendmentor.io?ref=challenge'
+				target='_blank'
+			>
+				Frontend Mentor
+			</a>
+			. Coded by{' '}
+			<a
+				href='https://www.frontendmentor.io/profile/benilio'
+				target='_blank'
+			>
+				Benicio Oliveira
+			</a>
+			.
+		</div>
+	)
+}
