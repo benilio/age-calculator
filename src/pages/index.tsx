a16890abf507e9c8771788d11fe84fdfd9cf8b8e
Benício Oliveira <beafoliveira@gmail.com>
2023-05-04 15:16:45 -0300

    undo components and commit mistakes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,7 @@ import Head from 'next/head'
 import { Poppins } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-import { Forms } from '@/components/Forms'
-import { Divider } from '@/components/Divider'
-import { Results } from '@/components/Results'
-import { Attribution } from '@/components/Attribution'
+import Image from 'next/image'
 
 const poppins = Poppins({
 	subsets: ['latin-ext'],
@@ -35,11 +32,97 @@ export default function Home() {
 			</Head>
 			<main className={`${styles.main} ${poppins.className}`}>
 				<section className={styles.calculator}>
-					<Forms />
-					<Divider />
-					<Results />
+					<form className={styles.form}>
+						<div className={styles.form__field}>
+							<label htmlFor='day'>Day</label>
+							<input
+								placeholder='DD'
+								type='number'
+								name='day'
+								id='day'
+							/>
+						</div>
+
+						<div className={styles.form__field}>
+							<label htmlFor='month'>Month</label>
+							<input
+								placeholder='MM'
+								type='number'
+								name='month'
+								id='month'
+							/>
+						</div>
+
+						<div className={styles.form__field}>
+							<label htmlFor='year'>Year</label>
+							<input
+								placeholder='YYYY'
+								type='number'
+								name='year'
+								id='year'
+							/>
+						</div>
+					</form>
+					<div className={styles.divider}>
+						<div className={styles.divider__bar}></div>
+						<div className={styles.divider__bar}></div>
+						<div className={styles.button}>
+							<Image
+								src='/icon-arrow.svg'
+								alt='icon arrow'
+								width={46}
+								height={46}
+								priority
+							/>
+						</div>
+					</div>
+					<div className={styles.result}>
+						<p>
+							<span
+								id='years'
+								className={styles.result__numbers}
+							>
+								--
+							</span>{' '}
+							years
+						</p>
+						<p>
+							<span
+								id='months'
+								className={styles.result__numbers}
+							>
+								--
+							</span>{' '}
+							months
+						</p>
+						<p>
+							<span
+								id='days'
+								className={styles.result__numbers}
+							>
+								--
+							</span>{' '}
+							days
+						</p>
+					</div>
 				</section>
-				<Attribution />
+				<div className={styles.attribution}>
+					Challenge by{' '}
+					<a
+						href='https://www.frontendmentor.io?ref=challenge'
+						target='_blank'
+					>
+						Frontend Mentor
+					</a>
+					. Coded by{' '}
+					<a
+						href='https://www.frontendmentor.io/profile/benilio'
+						target='_blank'
+					>
+						Benicio Oliveira
+					</a>
+					.
+				</div>
 			</main>
 		</>
 	)
