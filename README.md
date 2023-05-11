ab0b03829a0228b3d0e5b1d8d0ea22ad6eb34941
Benício Oliveira <beafoliveira@gmail.com>
2023-05-11 08:38:50 -0300

    Responsive css, numbers animation, README update

--- README.md
+++ README.md
@@ -1,38 +1,79 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Frontend Mentor - Age calculator app solution
 
-## Getting Started
+![application screenshot](public/screenshot.png)
 
-First, run the development server:
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q).
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+## Table of contents
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+## Overview
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+### The challenge
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+Users should be able to:
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
 
-## Learn More
+### Links
 
-To learn more about Next.js, take a look at the following resources:
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+## My process
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+### Built with
 
-## Deploy on Vercel
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktop-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Next.js](https://nextjs.org/) - React framework
+- [React Hook Form](https://react-hook-form.com/) - For form validation
+- [Zod](https://zod.dev/) - For form validation
+- [Day.js](https://day.js.org/) - For date manipulation
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+### What I learned
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+- React Hook Form
+- Zod
+- Day.js
+
+### Continued development
+
+Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
+
+### Useful resources
+
+- [Kevin Powell](https://www.youtube.com/@KevinPowell) - CSS
+- [Web Dev Simplified](https://www.youtube.com/@WebDevSimplified) - React/Zod
+- [Rocketseat](https://www.youtube.com/@rocketseat) - Zod
+- [Stackoverflow @nickf](https://stackoverflow.com/a/1433119) - Check date validity
+- [Stackoverflow @jfriend00](https://stackoverflow.com/a/16994725) - Animate numbers
+
+## Author
+
+- [@benilio](https://www.frontendmentor.io/profile/benilio) - Frontend Mentor
+- [Website](https://benilio.vercel.app/)
+- [LinkedIn Profile](https://br.linkedin.com/in/ben%C3%ADcio-oliveira-458158135)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,166 @@
 import Head from 'next/head'
-import { Poppins } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
 
 import Image from 'next/image'
 
-const poppins = Poppins({
-	subsets: ['latin-ext'],
-	weight: ['400', '700', '800', '900'],
-	style: ['normal', 'italic'],
-})
+// @ts-ignore
+import ArrowIconUrl from '@/assets/icon-arrow.svg?url'
+import styles from '@/styles/Home.module.css'
+
+// Import duration from dayjs
+dayjs.extend(duration)
+
+// Check days in month
+// https://stackoverflow.com/a/1433119
+function daysInMonth(m: number, y: number) {
+	// m is 0 indexed: 0-11
+	switch (m) {
+		case 1:
+			return (y % 4 == 0 && y % 100) || y % 400 == 0 ? 29 : 28
+		case 8:
+		case 3:
+		case 5:
+		case 10:
+			return 30
+		default:
+			return 31
+	}
+}
+
+// Check if the date is valid
+// https://stackoverflow.com/a/1433119
+function isValid(d: number, m: number, y: number) {
+	m = parseInt(m.toString(), 10) - 1
+	return m >= 0 && m < 12 && d > 0 && d <= daysInMonth(m, y)
+}
+
+// Form date schema with validation
+const createDateSchema = z
+	.object({
+		day: z
+			.number({
+				required_error: 'This field is required',
+				invalid_type_error: 'This field is required',
+			})
+			.int('Must be a valid day')
+			.positive('Must be a valid day')
+			.min(1, 'Must be a valid day')
+			.max(31, 'Must be a valid day'),
+		month: z
+			.number({
+				required_error: 'This field is required',
+				invalid_type_error: 'This field is required',
+			})
+			.int('Must be a valid month')
+			.positive('Must be a valid month')
+			.min(1, 'Must be a valid month')
+			.max(12, 'Must be a valid month'),
+		year: z
+			.number({
+				required_error: 'This field is required',
+				invalid_type_error: 'This field is required',
+			})
+			.int('Must be a valid year')
+			.positive('Must be a valid year')
+			.min(1, 'Must be a valid year')
+			.max(new Date().getFullYear(), 'Must be in the past'),
+	})
+	.refine((data) => isValid(data.day, data.month, data.year) === true, {
+		message: 'Must be a valid date',
+		path: ['day'],
+	})
+	.refine((data) => isValid(data.day, data.month, data.year) === true, {
+		message: ' ',
+		path: ['month'],
+	})
+	.refine((data) => isValid(data.day, data.month, data.year) === true, {
+		message: ' ',
+		path: ['year'],
+	})
+
+// Infer type of date inputs based on date schema
+type CreateDateData = z.infer<typeof createDateSchema>
 
 export default function Home() {
+	// Results holders
+	const [days, setDays] = useState('--')
+	const [months, setMonths] = useState('--')
+	const [years, setYears] = useState('--')
+
+	// Use react-hook-form
+	const {
+		register,
+		getValues,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<CreateDateData>({
+		resolver: zodResolver(createDateSchema),
+	})
+
+	// Form onSubmit response
+	function getDate() {
+		// Get values from form and turn into valid date
+		const day = getValues('day')
+		const month = getValues('month') - 1
+		const year = getValues('year')
+		const birthday = new Date(year, month, day)
+
+		// Get current day (simple)
+		const currentDay = new Date()
+		const today = new Date(
+			currentDay.getFullYear(),
+			currentDay.getMonth(),
+			currentDay.getDate()
+		)
+
+		// Get the difference between today date and set date
+		const dateDiff = [
+			dayjs.duration(dayjs(today).diff(dayjs(birthday))).days(),
+			dayjs.duration(dayjs(today).diff(dayjs(birthday))).months(),
+			dayjs.duration(dayjs(today).diff(dayjs(birthday))).years(),
+		]
+
+		// Animate results
+		// https://stackoverflow.com/a/16994725
+		let daysDiff: number = 0
+		let monthsDiff: number = 0
+		let yearsDiff: number = 0
+
+		const dateDiffAni = (
+			start: number,
+			end: number,
+			duration: number,
+			target: any
+		) => {
+			if (start === end) return
+			let range = end - start
+			let current = start
+			let increment = end > start ? 1 : -1
+			let stepTime = Math.abs(Math.floor(duration / range))
+			let timer = setInterval(function () {
+				current += increment
+				target(current.toString())
+				if (current == end) {
+					clearInterval(timer)
+				}
+			}, stepTime)
+		}
+
+		// Print results
+		dateDiffAni(daysDiff, dateDiff[0], 2000, setDays)
+		dateDiffAni(monthsDiff, dateDiff[1], 1000, setMonths)
+		dateDiffAni(yearsDiff, dateDiff[2], 3000, setYears)
+
+		return
+	}
+
 	return (
 		<>
 			<Head>
@@ -30,51 +180,87 @@ export default function Home() {
 					href='/favicon.png'
 				/>
 			</Head>
-			<main className={`${styles.main} ${poppins.className}`}>
+			<main className={`${styles.main}`}>
 				<section className={styles.calculator}>
-					<form className={styles.form}>
-						<div className={styles.form__field}>
+					<form
+						id='calcAgeForm'
+						onSubmit={handleSubmit(getDate)}
+						className={styles.form}
+					>
+						<div
+							className={
+								errors.day
+									? `${styles.form__field} ${styles.error}`
+									: `${styles.form__field}`
+							}
+						>
 							<label htmlFor='day'>Day</label>
 							<input
 								placeholder='DD'
 								type='number'
-								name='day'
 								id='day'
+								{...register('day', { valueAsNumber: true })}
 							/>
+							{errors.day && (
+								<span className={styles.error}>{errors.day.message}</span>
+							)}
 						</div>
 
-						<div className={styles.form__field}>
+						<div
+							className={
+								errors.month
+									? `${styles.form__field} ${styles.error}`
+									: `${styles.form__field}`
+							}
+						>
 							<label htmlFor='month'>Month</label>
 							<input
 								placeholder='MM'
 								type='number'
-								name='month'
 								id='month'
+								{...register('month', { valueAsNumber: true })}
 							/>
+							{errors.month && (
+								<span className={styles.error}>{errors.month.message}</span>
+							)}
 						</div>
 
-						<div className={styles.form__field}>
+						<div
+							className={
+								errors.year
+									? `${styles.form__field} ${styles.error}`
+									: `${styles.form__field}`
+							}
+						>
 							<label htmlFor='year'>Year</label>
 							<input
 								placeholder='YYYY'
 								type='number'
-								name='year'
 								id='year'
+								{...register('year', { valueAsNumber: true })}
 							/>
+							{errors.year && (
+								<span className={styles.error}>{errors.year.message}</span>
+							)}
 						</div>
+
+						<div className={styles.spacer}>empty</div>
 					</form>
+
 					<div className={styles.divider}>
-						<div className={styles.divider__bar}></div>
-						<div className={styles.divider__bar}></div>
-						<div className={styles.button}>
+						<div className={`${styles.divider__bar} ${styles.left}`}></div>
+						<div className={`${styles.divider__bar} ${styles.right}`}></div>
+						<button
+							type='submit'
+							form='calcAgeForm'
+							className={styles.button}
+						>
 							<Image
-								src='/icon-arrow.svg'
-								alt='icon arrow'
-								width={46}
-								height={46}
+								src={ArrowIconUrl}
+								alt='Arrow down'
 								priority
 							/>
-						</div>
+						</button>
 					</div>
 					<div className={styles.result}>
 						<p>
@@ -82,7 +268,7 @@ export default function Home() {
 								id='years'
 								className={styles.result__numbers}
 							>
-								--
+								{years}
 							</span>{' '}
 							years
 						</p>
@@ -91,7 +277,7 @@ export default function Home() {
 								id='months'
 								className={styles.result__numbers}
 							>
-								--
+								{months}
 							</span>{' '}
 							months
 						</p>
@@ -100,7 +286,7 @@ export default function Home() {
 								id='days'
 								className={styles.result__numbers}
 							>
-								--
+								{days}
 							</span>{' '}
 							days
 						</p>
@@ -119,7 +305,7 @@ export default function Home() {
 						href='https://www.frontendmentor.io/profile/benilio'
 						target='_blank'
 					>
-						Benicio Oliveira
+						Benício Oliveira
 					</a>
 					.
 				</div>
